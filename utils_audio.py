0fe73bbf612a862405ea23af86305320040a9fbb
ephell <dremaitiserikas@yahoo.com>
2023-04-28 18:08:26 +0300

    Add get_default_loopback_device()

--- utils_audio.py
+++ utils_audio.py
@@ -23,7 +23,25 @@ class AudioUtils:
                 except OSError:
                     return None
         return None
-            
+
+    @classmethod
+    def get_default_loopback_device(cls):
+        default_speakers = AudioUtils.get_default_speakers()
+        if default_speakers is not None:
+            with pyaudio.PyAudio() as p:
+                for loopback_device in p.get_loopback_device_info_generator():
+                    if default_speakers["name"] in loopback_device["name"]:
+                        return loopback_device
+        return None
+
+    @classmethod
+    def get_WASAPI_info(cls):
+        with pyaudio.PyAudio() as p:
+            try:
+                return p.get_host_api_info_by_type(pyaudio.paWASAPI)
+            except OSError:
+                return False
+
     @classmethod
     def is_WASAPI_available(cls):
         with pyaudio.PyAudio() as p:
@@ -32,11 +50,3 @@ class AudioUtils:
                 if "wasapi" in api["name"].lower():
                     return True
             return False
-            
-    @classmethod
-    def get_WASAPI_info(cls):
-        with pyaudio.PyAudio() as p:
-            try:
-                return p.get_host_api_info_by_type(pyaudio.paWASAPI)
-            except OSError:
-                return False
