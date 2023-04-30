570b4b6358a61a70a08fc58c6e0ab475c69728c9
ephell <dremaitiserikas@yahoo.com>
2023-04-30 03:34:37 +0300

    - Clean up imports. - Add **kwargs support to constructor. - Initialize recorders in constructor. - Add __initialize_loopback_recorder(). - Add __initialize_microphone_recorder(). - Add __get_recorders().

--- recorder.py
+++ recorder.py
@@ -1,9 +1,11 @@
 import datetime
-import os
-import recorder_video as rv
-import recorder_audio as ra
 import multiprocessing as mp
+import os
+
 from encoder import Encoder
+from recorder_loopback import LoopbackRecorder
+from recorder_microphone import MicrophoneRecorder
+from recorder_video import VideoRecorder
 from utils_audio import AudioUtils
 
 
@@ -15,55 +17,81 @@ class Recorder:
             duration, 
             record_video,
             record_loopback,
-            record_microphone
+            record_microphone,
+            **kwargs
         ):
         self.duration = duration
         self.record_video = record_video
         self.record_loopback = record_loopback
         self.record_microphone = record_microphone
-
-        MONITOR = 2
-        # REGION = [0, 0, 1920, 1080]
-        REGION = [60, 216, 1150, 650]
-        FPS = 30
-
-        barrier = mp.Barrier(3)
+        self.__unpack_kwargs(kwargs)
+        self.video_recorder = None
+        self.loopback_recorder = None
+        self.microphone_recorder = None
 
         if self.record_video:
-            self.video_recorder = rv.VideoRecorder(
-                monitor=MONITOR, 
-                region=REGION,
-                duration=self.duration,
-                fps=FPS,
-                barrier=barrier
-            )
+            self.__initialize_video_recorder()
+        if self.record_loopback:
+            self.__initialize_loopback_recorder()
+        if self.record_microphone:
+            self.__initialize_microphone_recorder()
 
-        if self.record_loopback or self.record_microphone:
-            lb_device = AudioUtils.get_default_loopback_device()
-            microphone = AudioUtils.get_default_microphone()
-
-            self.record_loopback = True if lb_device is not None else False
-            self.record_microphone = True if microphone is not None else False
-
-            self.audio_recorder = ra.AudioRecorder(
-                duration=self.duration,
-                loopback_device=lb_device if self.record_loopback else None,
-                microphone=microphone if self.record_microphone else None,
-                barrier=barrier
-            )
+        # Make sure all recorders start at the same time.
+        barrier = mp.Barrier(len(self.__get_recorders()))
+        for recorder in self.__get_recorders():
+            recorder.barrier = barrier
 
     def record(self):
-        if self.video_recorder is not None:
-            self.video_recorder.start()
-
-        if self.audio_recorder is not None:
-            self.audio_recorder.record()
-        self.video_recorder.join()
-
+        for recorder in self.__get_recorders():
+            recorder.start()
+        for recorder in self.__get_recorders():
+            recorder.join()
         self.__generate_final_video()
         self.__clean_up_temp_directory()
 
-        print("All done!")
+    def __unpack_kwargs(self, kwargs):
+        self.monitor = kwargs.get("monitor", 1)
+        self.region = kwargs.get("region", [0, 0, 1920, 1080])
+        self.fps = kwargs.get("fps", 30)
+
+    def __initialize_video_recorder(self):
+        if self.record_video:
+            self.video_recorder = VideoRecorder(
+                monitor=self.monitor, 
+                region=self.region,
+                duration=self.duration,
+                fps=self.fps,
+            )
+
+    def __initialize_loopback_recorder(self):
+        loopback_device = AudioUtils.get_default_loopback_device()
+        if loopback_device is not None:
+            self.record_loopback = True
+            self.loopback_recorder = LoopbackRecorder(
+                loopback_device=loopback_device,
+                duration=self.duration,
+            )
+        else:
+            self.record_loopback = False
+
+    def __initialize_microphone_recorder(self):
+        microphone = AudioUtils.get_default_microphone()
+        if microphone is not None:
+            self.record_microphone = True
+            self.microphone_recorder = MicrophoneRecorder(
+                microphone=microphone,
+                duration=self.duration,
+            )
+        else:
+            self.record_microphone = False
+
+    def __get_recorders(self):
+        recorders = [
+            self.video_recorder, 
+            self.loopback_recorder, 
+            self.microphone_recorder
+        ]
+        return [recorder for recorder in recorders if recorder is not None]
 
     def __generate_final_video(self):
         """Generates the final file from recorded temporary files."""
