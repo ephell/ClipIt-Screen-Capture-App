b57a2a46915529323ca70ada01d347e9d76ee8b8
ephell <dremaitiserikas@yahoo.com>
2023-05-02 19:15:52 +0300

    Add generation of recording area border

--- src/gui/recording_area_border.py
+++ src/gui/recording_area_border.py
@@ -1,30 +1,29 @@
 import win32api, win32con, win32gui
 
-class MyWindow:
+
+class RecordingAreaBorder:
 
     def __init__(self):
         win32gui.InitCommonControls()
 
-        dwExStyle = win32con.WS_EX_LAYERED
+        # Add 'win32con.WS_EX_TOOLWINDOW' to hide the window from the taskbar.
+        dwExStyle = win32con.WS_EX_LAYERED | win32con.WS_EX_TOOLWINDOW
         lpClassName = "MyWndClass"
         lpWindowName = "WINDOW"
-        dwStyle = win32con.WS_POPUP | win32con.WS_VISIBLE | win32con.WS_THICKFRAME
+        dwStyle = win32con.WS_VISIBLE | win32con.WS_POPUP | win32con.WS_BORDER
         top_left_x = 0
         top_left_y = 0
-        nWidth = 500
-        nHeight = 500
+        nWidth = 800
+        nHeight = 800
         hwndParent = 0
         hMenu = 0
         hInstance = win32api.GetModuleHandle(None)
         lpParam = None
 
-        message_map = {
-            win32con.WM_DESTROY: self.__on_destroy,
-            win32con.WM_NCHITTEST: self.__on_nchittest,
-        }
+        message_handler = {win32con.WM_DESTROY: self.__on_destroy}
         wc = win32gui.WNDCLASS()
         wc.style = win32con.CS_HREDRAW | win32con.CS_VREDRAW
-        wc.lpfnWndProc = message_map
+        wc.lpfnWndProc = message_handler
         wc.lpszClassName = lpClassName
         win32gui.RegisterClass(wc)
 
@@ -42,25 +41,28 @@ class MyWindow:
             hInstance,
             lpParam
         )
+        self.__set_window_region(self.hwnd)
+        self.__set_window_always_on_top(self.hwnd)
         win32gui.SetLayeredWindowAttributes(self.hwnd, 0, 128, win32con.LWA_ALPHA)
         win32gui.UpdateWindow(self.hwnd)
         win32gui.ShowWindow(self.hwnd, win32con.SW_SHOW)
+
+    def __set_window_always_on_top(self, hwnd):
+        win32gui.SetWindowPos(
+            hwnd, win32con.HWND_TOPMOST, 0, 0, 0, 0,
+            win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_SHOWWINDOW
+        )
+
+    def __set_window_region(self, hwnd):
+        region = win32gui.CreateRoundRectRgn(0, 0, 800, 800, 0, 0)
+        exclude = win32gui.CreateRoundRectRgn(10, 10, 790, 790, 0, 0)
+        win32gui.CombineRgn(region, region, exclude, win32con.RGN_DIFF)
+        win32gui.SetWindowRgn(hwnd, region, True)
 
     def __on_destroy(self, hwnd, message, wparam, lparam):
         win32gui.PostQuitMessage(0)
         return True
 
-    def __on_nchittest(self, hwnd, message, wparam, lparam):
-        x, y = win32api.LOWORD(lparam), win32api.HIWORD(lparam)
-        if self.__is_point_in_client_rect(x, y):
-            return win32con.HTTRANSPARENT
-        else:
-            return win32con.HTCLIENT
 
-    def __is_point_in_client_rect(self, x, y):
-        rect = win32gui.GetClientRect(self.hwnd)
-        left, top, right, bottom = rect
-        return left <= x < right and top <= y < bottom
-
-w = MyWindow()
+w = RecordingAreaBorder()
 win32gui.PumpMessages()
