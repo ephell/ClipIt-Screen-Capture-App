85bcd022ffa92fa8e50099aa3768022a7e986b1a
ephell <dremaitiserikas@yahoo.com>
2023-05-03 18:27:25 +0300

    Combine SelectionArea and MousePositionTracker

--- src/gui/region_selection.py
+++ src/gui/region_selection.py
@@ -4,42 +4,7 @@ import mss
 from PIL import Image, ImageTk, ImageEnhance
 
 
-class MousePositionTracker:
-
-    def __init__(self, canvas):
-        self.canvas = canvas
-        self.left_click_coords = None
-        self.left_drag_coords = None
-        self.reset()
-
-    def get_selection_coords(self):
-        return (self.left_click_coords, self.left_drag_coords)
-
-    def __on_left_mouse_click(self, event):
-        self.left_click_coords = (event.x, event.y)  
-
-    def __on_left_mouse_drag(self, event):
-        self.left_drag_coords = (event.x, event.y)
-        # User callback.
-        self._command(self.left_click_coords, self.left_drag_coords) 
-
-    def reset(self):
-        self.left_click_coords = self.left_drag_coords = None
-
-    def autodraw(self, command=lambda *args: None):
-        """Setup automatic drawing; supports command option"""
-        self.reset()
-        self._command = command
-        self.canvas.bind("<Button-1>", self.__on_left_mouse_click)
-        self.canvas.bind("<B1-Motion>", self.__on_left_mouse_drag)
-        self.canvas.bind("<ButtonRelease-1>", self.quit)
-
-    def quit(self, event):
-        self.reset()
-
-
-class SelectionAreaDrawer:
-    """Widget to display a rectangular selection area on a canvas."""
+class AreaSelector:
 
     def __init__(self, canvas):
         self.canvas = canvas
@@ -52,18 +17,33 @@ class SelectionAreaDrawer:
             outline="white",
             state=tk.HIDDEN
         )
+        self.left_click_coords = None
+        self.left_drag_coords = None
 
-    def draw(self, start, end):
-        """Draw a rectangular area on the canvas."""
-        x0, y0, x1, y1 = self.__get_coords(start, end)
+    def start(self):
+        self.canvas.bind("<Button-1>", self.__on_left_mouse_click)
+        self.canvas.bind("<B1-Motion>", self.__on_left_mouse_drag)
+        self.canvas.bind("<ButtonRelease-1>", self.__on_left_mouse_release)
+
+    def get_selection_coords(self):
+        return (self.left_click_coords, self.left_drag_coords)
+
+    def __on_left_mouse_click(self, event):
+        self.left_click_coords = (event.x, event.y)  
+
+    def __on_left_mouse_drag(self, event):
+        self.left_drag_coords = (event.x, event.y)
+        self.__draw(self.left_click_coords, self.left_drag_coords) 
+
+    def __on_left_mouse_release(self, event):
+        print(self.get_selection_coords())
+
+    def __draw(self, start, end):
+        x0, y0, x1, y1 = self.__determine_corners(start, end)
         self.canvas.coords(self.selection_area, x0, y0, x1, y1),
         self.canvas.itemconfigure(self.selection_area, state=tk.NORMAL)
 
-    def __get_coords(self, start, end):
-        """ 
-        Determine top left and bottom right coordinates of the selection
-        area.
-        """
+    def __determine_corners(self, start, end):
         return (min((start[0], end[0])), min((start[1], end[1])),
                 max((start[0], end[0])), max((start[1], end[1])))
 
@@ -97,16 +77,8 @@ class Application:
         self.canvas.image = self.screenshot
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.canvas.image)
 
-        # Create selection object to show current selection boundaries.
-        self.selection_area = SelectionAreaDrawer(self.canvas)
-
-        # Callback function to update it given two points of its diagonal.
-        def on_drag(start, end):  # Must accept these arguments.
-            self.selection_area.draw(start, end)
-
-        # Create mouse position tracker that uses the function.
-        self.posn_tracker = MousePositionTracker(self.canvas)
-        self.posn_tracker.autodraw(command=on_drag)  # Enable callbacks.
+        self.sa = AreaSelector(self.canvas)
+        self.sa.start()
 
     def run(self):
         self.root.mainloop()
