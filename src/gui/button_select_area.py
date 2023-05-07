91ec218d1dbb33cc42f0154a7ecb247c19c11947
ephell <dremaitiserikas@yahoo.com>
2023-05-07 07:44:04 +0300

    Move select area button to it's own module

---
+++ src/gui/button_select_area.py
@@ -0,0 +1,98 @@
+from logger import GlobalLogger
+log = GlobalLogger.LOGGER
+
+import mss
+from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QLabel
+
+from gui.recording_area_border import RecordingAreaBorder
+from gui.area_selector import AreaSelector
+
+
+class SelectAreaButton(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        self.area_selector = None
+        self.recording_area_border = None
+        self.recording_area_top_x = None
+        self.recording_area_top_y = None
+        self.recording_area_bottom_x = None
+        self.recording_area_bottom_y = None
+
+        button = QPushButton()
+        button.setText("Select Area")
+        button.clicked.connect(self.__on_select_area_clicked)
+
+        self.area_label = QLabel()
+        self.area_label.setText(f"No area selected.")
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.area_label)
+        layout.addWidget(button)
+        self.setLayout(layout)
+
+    def get_area_coords(self):
+        return (
+            self.recording_area_top_x,
+            self.recording_area_top_y,
+            self.recording_area_bottom_x,
+            self.recording_area_bottom_y
+        )
+
+    def update_area_label(self, area_coords=None):
+        if area_coords is not None:
+            self.area_label.setText(f"Area: {area_coords}")
+        else:
+            self.area_label.setText(f"No area selected.")
+
+    def __on_select_area_clicked(self):
+        """Callback function for the select area button."""
+        if self.recording_area_border is not None:
+            self.recording_area_border.destroy()
+        self.area_selector = AreaSelector(
+            self.__get_area_coords,
+            self.update_area_label
+        )
+        self.area_selector.show()
+
+    def __get_area_coords(self, x0, y0, x1, y1):
+        """Callback function for the area selector."""
+        if not self.__is_within_single_monitor_bounds(x0, y0, x1, y1):
+            log.error("Selected area must be within a single monitor.")
+            self.area_selector.close()
+            return
+        
+        self.update_area_label((x0, y0, x1, y1))
+        self.recording_area_top_x = x0
+        self.recording_area_top_y = y0
+        self.recording_area_bottom_x = x1
+        self.recording_area_bottom_y = y1
+        self.area_selector.close()
+        self.__draw_recording_area_border(x0, y0, x1, y1)
+
+    def __is_within_single_monitor_bounds(self, x0, y0, x1, y1):
+        """
+        Check if top left and bottom right coordinates of the area 
+        are within bounds of a single monitor.
+        """
+        with mss.mss() as sct:
+            monitors = sct.monitors
+            monitor_index_1 = None
+            monitor_index_2 = None
+            for i in range(1, len(monitors)):
+                m = monitors[i]
+                if (m["left"] <= x0 < m["left"] + m["width"] 
+                    and m["top"] <= y0 < m["top"] + m["height"]):
+                    monitor_index_1 = i
+                if (m["left"] <= x1 < m["left"] + m["width"] 
+                    and m["top"] <= y1 < m["top"] + m["height"]):
+                    monitor_index_2 = i
+
+            if monitor_index_1 == monitor_index_2:
+                return True
+            else:
+                return False
+
+    def __draw_recording_area_border(self, x0, y0, x1, y1):
+        self.recording_area_border = RecordingAreaBorder(x0, y0, x1, y1)
+        self.recording_area_border.start()
--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,18 +1,15 @@
 from logger import GlobalLogger
 log = GlobalLogger.LOGGER
 
-import mss
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
     QWidget,
     QMainWindow,
     QPushButton,
-    QVBoxLayout,
-    QLabel,
+    QVBoxLayout
 )
 
-from gui.recording_area_border import RecordingAreaBorder
-from gui.region_selector import RegionSelector
+from gui.button_select_area import SelectAreaButton
 from recorder.recorder import Recorder
 
 
@@ -23,16 +20,8 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("My App")
         self.app = app
-        self.region_selector = None
-        self.recording_area_border = None
-        self.recording_area_top_x = None
-        self.recording_area_top_y = None
-        self.recording_area_bottom_x = None
-        self.recording_area_bottom_y = None
-
-        select_region = QPushButton()
-        select_region.setText("Select Area")
-        select_region.clicked.connect(self.__on_select_region_clicked)
+        
+        self.select_area_button = SelectAreaButton()
 
         start_recording_button = QPushButton()
         start_recording_button.setText("Start Recording")
@@ -42,14 +31,10 @@ class MainWindow(QMainWindow):
         stop_recording_button.setText("Stop Recording")
         stop_recording_button.clicked.connect(self.__on_stop_clicked)
 
-        self.region_label = QLabel()
-        self.region_label.setText(f"No region selected.")
-
         layout = QVBoxLayout()
-        layout.addWidget(select_region)
+        layout.addWidget(self.select_area_button)
         layout.addWidget(start_recording_button)
         layout.addWidget(stop_recording_button)
-        layout.addWidget(self.region_label)
 
         container = QWidget()
         container.setLayout(layout)
@@ -58,74 +43,26 @@ class MainWindow(QMainWindow):
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
-            if self.region_selector is not None:
-                self.region_selector.close()
-
-    def __on_select_region_clicked(self):
-        """Callback function for the select region button."""
-        def get_region(x0, y0, x1, y1):
-            """Callback function for the region selector."""
-            if not is_within_single_monitor_bounds(x0, y0, x1, y1):
-                log.error("Region must be within a single monitor.")
-                self.region_selector.close()
-                return
-            
-            self.region_label.setText(f"Region: ({x0}, {y0}, {x1}, {y1})")
-            self.region_selector.close() 
-            draw_recording_area_border(x0, y0, x1, y1)
-            self.recording_area_top_x = x0
-            self.recording_area_top_y = y0
-            self.recording_area_bottom_x = x1
-            self.recording_area_bottom_y = y1
-
-        def is_within_single_monitor_bounds(x0, y0, x1, y1):
-            with mss.mss() as sct:
-                monitors = sct.monitors
-                monitor_index_1 = None
-                monitor_index_2 = None
-                for i in range(1, len(monitors)):
-                    m = monitors[i]
-                    if (m["left"] <= x0 < m["left"] + m["width"] 
-                        and m["top"] <= y0 < m["top"] + m["height"]):
-                        monitor_index_1 = i
-                    if (m["left"] <= x1 < m["left"] + m["width"] 
-                        and m["top"] <= y1 < m["top"] + m["height"]):
-                        monitor_index_2 = i
-
-                if monitor_index_1 == monitor_index_2:
-                    return True
-                else:
-                    return False
-
-        def draw_recording_area_border(x0, y0, x1, y1):
-            self.recording_area_border = RecordingAreaBorder(x0, y0, x1, y1)
-            self.recording_area_border.start()
-
-        if self.recording_area_border is not None:
-            self.recording_area_border.destroy()
-        self.region_selector = RegionSelector(get_region)
-        self.region_selector.show()
+            if self.select_area_button.area_selector is not None:
+                self.select_area_button.update_area_label()
+                self.select_area_button.area_selector.close()
 
     def __on_stop_clicked(self):
-        if self.recording_area_border is not None:
-            self.recording_area_border.destroy()
-            self.recording_area_border = None
+        if self.select_area_button.recording_area_border is not None:
+            self.select_area_button.recording_area_border.destroy()
+            self.select_area_button.recording_area_border = None
+            self.select_area_button.update_area_label()
 
     def __on_start_clicked(self):
-        if self.recording_area_border is not None:
+        if self.select_area_button.recording_area_border is not None:
             recorder = Recorder(
-                duration=5,
+                duration=3,
                 record_video=True,
                 record_loopback=True,
                 record_microphone=True,
-                region=[
-                    self.recording_area_top_x,
-                    self.recording_area_top_y,
-                    self.recording_area_bottom_x - self.recording_area_top_x,
-                    self.recording_area_bottom_y - self.recording_area_top_y
-                ],
+                region=[*self.select_area_button.get_area_coords()],
                 fps=30,
             )
             recorder.start()
         else:
-            log.error("No region selected.")
+            log.error("Recording area not selected.")
