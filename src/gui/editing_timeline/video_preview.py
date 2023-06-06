9f545026e6ae5600bfc410c02c51dbb2b0d49c32
ephell <dremaitiserikas@yahoo.com>
2023-06-06 21:49:45 +0300

    Improve playback quality; enable looping

--- src/gui/editing_timeline/video_preview.py
+++ src/gui/editing_timeline/video_preview.py
@@ -1,23 +1,55 @@
 import sys
-from PySide6.QtWidgets import QApplication, QGraphicsView, QGraphicsScene
-from PySide6.QtMultimedia import QMediaPlayer
-from PySide6.QtCore import QUrl
-from PySide6.QtMultimediaWidgets import QGraphicsVideoItem
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtMultimedia import *
+from PySide6.QtMultimediaWidgets import *
+
+
+class VideoGraphicsView(QGraphicsView):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setRenderHint(QPainter.Antialiasing)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setAlignment(Qt.AlignCenter)
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        self.fitInView(self.sceneRect(), Qt.KeepAspectRatio)
+
+
+class VideoPlayer(QMediaPlayer):
+    def __init__(self):
+        super().__init__()
+
+    def restartPlayback(self, mediaStatus):
+        if mediaStatus == QMediaPlayer.EndOfMedia:
+            self.setPosition(0)  # Restart playback from the beginning
+            self.play()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    player = QMediaPlayer()
+    player = VideoPlayer()
     videoItem = QGraphicsVideoItem()
     player.setVideoOutput(videoItem)
 
-
     scene = QGraphicsScene()
     scene.addItem(videoItem)
-    view = QGraphicsView(scene)
+
+    view = VideoGraphicsView(scene)
+    view.resize(800, 600)
     view.show()
 
-    player.setSource(QUrl("src/test.mp4"))
+    player.setSource(QUrl("src/gui/editing_timeline/test.mp4"))
     player.play()
+
+    # Set the initial size of the video item to match the size of the QGraphicsView
+    videoItem.setSize(view.size())
+
+    # Connect the mediaStatusChanged signal to restartPlayback slot
+    player.mediaStatusChanged.connect(player.restartPlayback)
 
     sys.exit(app.exec())
