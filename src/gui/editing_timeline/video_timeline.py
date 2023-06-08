32498ba61c83703cfe537b35b3a437ebe85ee538
ephell <dremaitiserikas@yahoo.com>
2023-06-08 22:28:18 +0300

    Resize align editable media item with left border of graphicsView

--- src/gui/editing_timeline/video_timeline.py
+++ src/gui/editing_timeline/video_timeline.py
@@ -4,62 +4,81 @@ from PySide6.QtGui import *
 
 
 class TimelineItem(QGraphicsRectItem):
-    def __init__(self, view, duration):
+    def __init__(self, sceneRect):
         super().__init__()
-        self.view = view
+        self.sceneWidth = sceneRect.width()
+        self.sceneHeight = sceneRect.height()
         self.setFlag(QGraphicsRectItem.ItemIsMovable, True)
-        self.resizeBasedOnDuration(self.view.height(), duration)
+        self.setRect(0, 0, self.sceneWidth, self.sceneHeight/2)
+        self.moveToMiddle()
 
-    def resizeBasedOnDuration(self, height, duration):
-        scene_width = self.view.width()
-        duration_sec = duration / 1000
-        item_width = scene_width * duration_sec / 10
-        self.setRect(0, 0, item_width, height - 50)
-
+    """Override"""
     def paint(self, painter, option, widget):
         painter.setPen(QPen(Qt.red))
         painter.setBrush(QBrush(Qt.red))
         painter.drawRect(self.rect())
 
+    def moveToMiddle(self):
+        item_height = self.rect().height()
+        padding = (self.sceneHeight - item_height) / 2
+        self.setPos(0, padding)
+
 
 class TimelineScene(QGraphicsScene):
-    def __init__(self):
+    def __init__(self, mediaDuration):
         super().__init__()
+        if mediaDuration is not None:
+            self.setSceneRect(0, 0, mediaDuration/10, 150)
+        else:
+            self.setSceneRect(0, 0, 150, 150)
 
 
 class TimelineView(QGraphicsView):
     def __init__(self, scene=None):
         super().__init__()
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setScene(scene)
         self.setFixedHeight(150)
 
+    """Override"""
+    def showEvent(self, event):
+        super().showEvent(event)
+        self.equalizeSceneAndViewDimensions()
 
-class LoadedMediaWidget(QWidget):
-    """Main widget."""
-    def __init__(self, mediaDuration):
-        super().__init__()
+    """Override"""
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        self.equalizeSceneAndViewDimensions()
 
-        self.scene = TimelineScene()
-        self.view = TimelineView(self.scene)
-
-        self.timeline_item = TimelineItem(self.view, mediaDuration)
-        self.scene.addItem(self.timeline_item)
+    def equalizeSceneAndViewDimensions(self):
+        self.scene().setSceneRect(0, 0, self.width(), self.height())
 
 
 class MainWindow(QMainWindow):
     """For testing."""
-    def __init__(self):
+    def __init__(self, mediaDuration=None):
         super().__init__()
-        self.setWindowTitle("Test")
 
-        self.scene = TimelineScene()
+        self.scene = TimelineScene(mediaDuration)
         self.view = TimelineView(self.scene)
 
-        self.timeline_item = TimelineItem(self.view, 5000)
+        self.timeline_item = TimelineItem(self.scene.sceneRect())
         self.scene.addItem(self.timeline_item)
 
         self.setCentralWidget(self.view)
+
+
+class LoadedMediaWidget(QWidget):
+    """Main widget."""
+    def __init__(self, mediaDuration=None):
+        super().__init__()
+
+        self.scene = TimelineScene(mediaDuration)
+        self.view = TimelineView(self.scene)
+
+        self.timeline_item = TimelineItem(self.scene.sceneRect())
+        self.scene.addItem(self.timeline_item)
 
 
 if __name__ == '__main__':
