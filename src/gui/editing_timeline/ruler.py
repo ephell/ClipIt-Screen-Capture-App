8a67ad84b723025ba57be568eb9f6ed5c5e777fa
ephell <dremaitiserikas@yahoo.com>
2023-06-14 18:05:59 +0300

    Improve tick mark and label drawing on ruler

--- src/gui/editing_timeline/ruler.py
+++ src/gui/editing_timeline/ruler.py
@@ -1,39 +1,35 @@
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from PySide6.QtGui import *
-from PySide6.QtMultimedia import *
-from PySide6.QtMultimediaWidgets import *
-
 
 class Ruler(QGraphicsWidget):
     
-    def __init__(self):
+    def __init__(self, media_duration):
         super().__init__()
-        self.width = 750
-        self.height = 10
+        self.media_duration = media_duration
+        self.width = None
+        self.height = None
         
-    """Override"""
     def paint(self, painter, option, widget):
         super().paint(painter, option, widget)
         tick_positions = self.calculate_tick_positions()
 
         # Set the font for the tick labels
         font = painter.font()
-        font.setPointSize(10)
+        font.setPointSize(8)
         painter.setFont(font)
 
         pen = QPen(Qt.black)
         pen.setWidth(2)
         painter.setPen(pen)
-
+        
         # Draw the tick marks and labels
         for tick_pos in tick_positions:
-        
             # Draw the tick mark
             painter.drawLine(tick_pos, 0, tick_pos, self.height)
 
             # Draw the tick label
-            text = str(tick_pos)
+            text = self.generate_tick_label(tick_pos)
             text_rect = painter.fontMetrics().boundingRect(text)
             text_width = text_rect.width()
             text_height = text_rect.height()
@@ -42,11 +38,20 @@ class Ruler(QGraphicsWidget):
             painter.drawText(text_x, text_y, text)
 
     def calculate_tick_positions(self):
-        interval = 50
-        return [i * interval for i in range(int(self.width / interval) + 1)]
+        num_ticks = 10
+        tick_interval = self.width / num_ticks
+        return [int(i * tick_interval) for i in range(num_ticks + 1)]
+
+    def generate_tick_label(self, tick_pos):
+        time_value = tick_pos * self.media_duration / self.width
+        milliseconds = int(time_value)
+        seconds, milliseconds = divmod(milliseconds, 1000)
+        minutes, seconds = divmod(seconds, 60)
+        return f"{minutes:02d}:{seconds:02d}:{milliseconds:02d}"
     
     """Override"""
     def sizeHint(self, which, constraint):
-        # Has extra padding to the width so that last tick label
-        # is not cut off when the handle is dragged over it.
-        return QSizeF(self.width + 25, self.height)
+        # Takes into account the position of the widget inside the scene
+        self.width = int(self.scene().width() - self.pos().x() * 2)
+        self.height = 10
+        return QSizeF(self.width, self.height)
