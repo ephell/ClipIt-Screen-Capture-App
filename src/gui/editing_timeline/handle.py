22ca0ceebb1a9329b13a0dbbba416e4e6b8822ad
ephell <dremaitiserikas@yahoo.com>
2023-06-15 23:30:41 +0300

    Refactor RulerHandle for improved functionality
    - Change RulerHandle inheritance to QGraphicsItem.
    - Improve 'paint' method to draw a screw-like shape.
    - Update 'shape' method so it makes only the head of 'screw' clickable.
    - Update 'itemChange' method so that min and max X axis limits are respected.
    - Update constructor.
    - Update 'boundingRect' method. It now takes value from constructor.

--- src/gui/editing_timeline/handle.py
+++ src/gui/editing_timeline/handle.py
@@ -5,38 +5,57 @@ from PySide6.QtMultimedia import *
 from PySide6.QtMultimediaWidgets import *
 
 
-class RulerHandle(QGraphicsWidget):
+class RulerHandle(QGraphicsItem):
     
     def __init__(self, scene):
         super().__init__()
         self.scene = scene
         self.scene.addItem(self)
-        self.setPos(30, 0)
-        self.brush = QBrush(Qt.magenta)
-        self.pen = QPen(Qt.magenta)
-        self.rect = self.boundingRect()
         self.setFlag(QGraphicsWidget.ItemIsMovable, True)
-        self.setFlag(QGraphicsWidget.ItemIsSelectable, True)
+        self.setFlag(QGraphicsItem.ItemSendsScenePositionChanges)
+        self.width = 30
+        self.height = 100 + self.scene.media_item_y - self.scene.ruler_handle_y
+        self.rect = self.boundingRect()
+
+        self.head_width = self.width
+        self.head_height = self.height / 10
+        self.needle_width = 1
+        self.needle_height = self.height - self.head_height
+        self.needle_x = (self.width - self.needle_width) / 2
+        self.needle_y = self.head_height
+
+        self.minimum_x = self.scene.ruler_handle_x - self.width / 2
+        self.maximum_x = self.scene.width() - self.width * 2 + self.minimum_x
+
+        self.setPos(self.minimum_x, self.scene.ruler_handle_y)
+
 
     """Override"""
     def boundingRect(self):
-        return QRectF(0, 0, 1, 100)
+        return QRectF(0, 0, self.width, self.height)
 
     """Override"""
     def paint(self, painter, option, widget):
-        painter.setBrush(self.brush)
-        painter.setPen(self.pen)
-        painter.drawRect(self.boundingRect())
+        painter.setPen(QPen(Qt.magenta))
+        painter.setBrush(QBrush(Qt.magenta))
+        painter.drawRect(QRectF(0, 0, self.head_width, self.head_height))
+        painter.drawRect(QRectF(
+            self.needle_x, self.needle_y, self.needle_width, self.needle_height
+        ))
 
     """Override"""
     def itemChange(self, change, value):
         if change == QGraphicsItem.ItemPositionChange:
-            # Restrict movement to X-axis only
-            value.setY(self.pos().y())
+            current_x = value.x()
+            if current_x < self.minimum_x:
+                return QPointF(self.minimum_x, self.pos().y())
+            elif current_x > self.maximum_x:
+                return QPointF(self.maximum_x, self.pos().y())
+            return QPointF(value.x(), self.pos().y())
         return super().itemChange(change, value)
     
     """Override"""
     def shape(self):
         path = QPainterPath()
-        path.addRect(self.boundingRect())
+        path.addRect(0, 0, self.head_width, self.head_height)
         return path
