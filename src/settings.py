ef48137456c32d07dcc3a1955c57abe065e94646
ephell <dremaitiserikas@yahoo.com>
2023-07-07 20:18:43 +0300

    Add reencoded video file name constant to TempFiles

--- src/settings.py
+++ src/settings.py
@@ -13,6 +13,7 @@ class TempFiles:
     """Names of temporary files."""
 
     CAPTURED_VIDEO_FILE = "captured_video.mp4"
+    REENCODED_VIDEO_FILE = "reencoded_video.mp4"
     LOOPBACK_AUDIO_FILE = "loopback_audio.wav"
     MICROPHONE_AUDIO_FILE = "microphone_audio.wav"
     MERGED_AUDIO_FILE = "merged_audio.wav"
