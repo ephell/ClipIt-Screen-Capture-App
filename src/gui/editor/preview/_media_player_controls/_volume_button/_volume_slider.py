e6385cabbefa3cb7c90267a6693992e9595d8fd2
ephell <dremaitiserikas@yahoo.com>
2023-07-28 01:54:20 +0300

    Make volume slider handle jump to cursor pos on left click; Make handle draggable from any click pos when left mouse is pressed in

--- src/gui/editor/preview/_media_player_controls/_volume_button/_volume_slider.py
+++ src/gui/editor/preview/_media_player_controls/_volume_button/_volume_slider.py
@@ -11,3 +11,27 @@ class VolumeSlider(QSlider):
         self.setValue(100)
         self.setTickInterval(10)
         self.setPageStep(10)
+        self.dragging = False
+
+    """Override"""
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.dragging = True
+            self.setValue(self.__calculate_new_slider_value(event))
+        super().mousePressEvent(event)
+
+    """Override"""
+    def mouseReleaseEvent(self, event):
+        self.dragging = False
+        super().mouseReleaseEvent(event)
+
+    """Override"""
+    def mouseMoveEvent(self, event):
+        if self.dragging:
+            self.setValue(self.__calculate_new_slider_value(event))
+        super().mouseMoveEvent(event)
+
+    def __calculate_new_slider_value(self, event):
+        slider_value_per_pixel  = (self.maximum() - self.minimum()) / self.height()
+        click_pos_y = event.y()
+        return self.maximum() - int(slider_value_per_pixel  * click_pos_y)
