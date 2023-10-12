8eb0de3c570ace2be0b2fbe2a732709f427dcf60
ephell <dremaitiserikas@yahoo.com>
2023-10-12 09:23:40 +0300

    Adjust title of Settings window widget

--- src/gui/settings/settings.py
+++ src/gui/settings/settings.py
@@ -10,6 +10,7 @@ class Settings(QWidget, Ui_Settings):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        self.setWindowTitle("ClipIt - Settings")
         self.setAttribute(Qt.WA_DeleteOnClose, True)
         self.setWindowFlag(Qt.Window)
         self.setWindowFlags(Qt.Window | Qt.WindowTitleHint | Qt.WindowCloseButtonHint)
