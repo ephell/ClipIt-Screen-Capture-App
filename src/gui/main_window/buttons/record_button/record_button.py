08d7c9c9906756b0cae829834febed5454cd79c5
ephell <dremaitiserikas@yahoo.com>
2023-10-12 15:59:57 +0300

    Move final_file_generation_dialog to record_button

--- src/gui/main_window/buttons/record_button/record_button.py
+++ src/gui/main_window/buttons/record_button/record_button.py
@@ -5,7 +5,7 @@ from PySide6.QtWidgets import QPushButton, QMessageBox, QMainWindow, QLabel
 
 import threading
 
-from gui.main_window.final_file_generation_dialog.final_file_generation_dialog import FinalFileGenerationDialog
+from .final_file_generation_dialog.final_file_generation_dialog import FinalFileGenerationDialog
 from settings.settings import Settings
 from recorder.recorder import Recorder
 from ._recording_area_selector import RecordingAreaSelector
