42bcadc42b57fc78c889a3eba99b4b98c18a85e9
ephell <dremaitiserikas@yahoo.com>
2023-10-26 16:36:11 +0300

    Add icons to tray menu actions

--- src/gui/main_window/system_tray/system_tray.py
+++ src/gui/main_window/system_tray/system_tray.py
@@ -1,4 +1,4 @@
-from PySide6.QtCore import Slot, Signal, Qt, QTimer
+from PySide6.QtCore import Slot, Signal, Qt
 from PySide6.QtGui import QIcon, QAction
 from PySide6.QtWidgets import QMenu, QSystemTrayIcon, QMessageBox
 
@@ -7,7 +7,8 @@ class SystemTray(QSystemTrayIcon):
 
     request_exit_signal = Signal()
 
-    __ICON_PATH = "src/gui/application/logo.svg"
+    __TRAY_ICON_PATH = "src/gui/application/logo.svg"
+    # Action texts
     __ACTION_START_RECORDING_TEXT = "Start Recording"
     __ACTION_STOP_RECORDING_TEXT = "Stop Recording"
     __ACTION_SCREENSHOT_TEXT = "Take A Screenshot"
@@ -15,14 +16,23 @@ class SystemTray(QSystemTrayIcon):
     __ACTION_OPEN_CAPTURE_FOLDER_TEXT = "Open Capture Folder"
     __ACTION_SETTINGS_TEXT = "Settings"
     __ACTION_EXIT_TEXT = "Exit"
+    # Action icon paths
+    __ACTION_START_RECORDING_ICON_PATH = "src/gui/main_window/system_tray/icons/action_start_stop_recording/1x/baseline_videocam_white_24dp.png"
+    __ACTION_STOP_RECORDING_ICON_PATH = "src/gui/main_window/system_tray/icons/action_start_stop_recording/1x/baseline_videocam_off_white_24dp.png"
+    __ACTION_SCREENSHOT_ICON_PATH = "src/gui/main_window/system_tray/icons/action_screenshot/1x/baseline_camera_alt_white_24dp.png"
+    __ACTION_OPEN_EDITOR_ICON_PATH = "src/gui/main_window/system_tray/icons/action_open_editor/1x/baseline_movie_edit_white_24dp.png"
+    __ACTION_OPEN_CAPTURE_FOLDER_ICON_PATH = "src/gui/main_window/system_tray/icons/action_open_capture_folder/1x/baseline_folder_white_24dp.png"
+    __ACTION_SETTINGS_ICON_PATH = "src/gui/main_window/system_tray/icons/action_settings/1x/baseline_settings_white_24dp.png"
+    __ACTION_EXIT_ICON_PATH = "src/gui/main_window/system_tray/icons/action_exit/1x/baseline_close_white_24dp.png"
 
     def __init__(self, main_window):
         super().__init__(main_window)
         self.main_window = main_window
-        self.setIcon(QIcon(self.__ICON_PATH))
+        self.setIcon(QIcon(self.__TRAY_ICON_PATH))
         self.__tray_menu = QMenu()
         self.__initialize_actions()
-        self.__add_actions_to_tray()
+        self.__add_actions_to_tray_menu()
+        self.__set_action_icons()
         self.setContextMenu(self.__tray_menu)
         self.__connect_signals_and_slots()
 
@@ -38,7 +48,7 @@ class SystemTray(QSystemTrayIcon):
         self.action_settings = QAction(self.__ACTION_SETTINGS_TEXT)
         self.action_exit = QAction(self.__ACTION_EXIT_TEXT)
 
-    def __add_actions_to_tray(self):
+    def __add_actions_to_tray_menu(self):
         self.__tray_menu.addAction(self.action_start_stop_recording)
         self.__tray_menu.addAction(self.action_screenshot)
         self.__tray_menu.addAction(self.action_open_editor)
@@ -46,6 +56,14 @@ class SystemTray(QSystemTrayIcon):
         self.__tray_menu.addAction(self.action_settings)
         self.__tray_menu.addSeparator()
         self.__tray_menu.addAction(self.action_exit)
+
+    def __set_action_icons(self):
+        self.action_start_stop_recording.setIcon(QIcon(self.__ACTION_START_RECORDING_ICON_PATH))
+        self.action_screenshot.setIcon(QIcon(self.__ACTION_SCREENSHOT_ICON_PATH))
+        self.action_open_editor.setIcon(QIcon(self.__ACTION_OPEN_EDITOR_ICON_PATH))
+        self.action_open_capture_folder.setIcon(QIcon(self.__ACTION_OPEN_CAPTURE_FOLDER_ICON_PATH))
+        self.action_settings.setIcon(QIcon(self.__ACTION_SETTINGS_ICON_PATH))
+        self.action_exit.setIcon(QIcon(self.__ACTION_EXIT_ICON_PATH))
 
     def __request_exit(self):
         msg_box = QMessageBox.question(
@@ -65,10 +83,12 @@ class SystemTray(QSystemTrayIcon):
     @Slot()
     def on_recording_started(self):
         self.action_start_stop_recording.setText(self.__ACTION_STOP_RECORDING_TEXT)
+        self.action_start_stop_recording.setIcon(QIcon(self.__ACTION_STOP_RECORDING_ICON_PATH))
 
     @Slot()
     def on_recording_stopped(self):
         self.action_start_stop_recording.setText(self.__ACTION_START_RECORDING_TEXT)
+        self.action_start_stop_recording.setIcon(QIcon(self.__ACTION_START_RECORDING_ICON_PATH))
 
     @Slot()
     def __on_tray_icon_activated(self, reason):
