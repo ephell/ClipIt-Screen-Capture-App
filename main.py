e90e15e1e1fb1a43463d98306ff5b070f0ada800
ephell <dremaitiserikas@yahoo.com>
2023-10-31 14:50:04 +0200

    Add freeze_support() call for multithreading support when application is frozen/converted into .exe

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from multiprocessing import freeze_support
 import sys
 
 from src.gui.application.application import Application
@@ -5,6 +6,7 @@ from src.gui.main_window.main_window import MainWindow
 
     
 if __name__ == "__main__":
+    freeze_support()
     app = Application(sys.argv)
     main_window = MainWindow(app)
     main_window.show()
