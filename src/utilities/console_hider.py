cca99b967dc563ecf9638e193c88f375150ecdc7
ephell <dremaitiserikas@yahoo.com>
2023-11-01 18:46:22 +0200

    Always hide kernel32 console hwnd and attempt to hide the win32gui one

--- src/utilities/console_hider.py
+++ src/utilities/console_hider.py
@@ -1,4 +1,6 @@
+import ctypes
 import threading
+from time import perf_counter, sleep
 
 import win32gui
 import win32con
@@ -6,19 +8,35 @@ import win32con
 
 class ConsoleHider(threading.Thread):
 
-    def __init__(self, console_title: str):
+    def __init__(self):
         super().__init__()
-        self.console_title = console_title
+        self.setDaemon(True)
+        self.__console_title = "ClipIt-Console"
+        ctypes.windll.kernel32.SetConsoleTitleW(self.__console_title)
+        self.__thread_timeout = 30
+        self.__is_hidden_hwnd_kernel32 = False
+        self.__is_hidden_hwnd_win32gui = False
 
     def run(self):
-        while True:
-            hwnd = self.__get_hwnd(self.console_title)
-            if hwnd is not None and hwnd != 0:
-                self.__make_fully_transparent(hwnd)
-                self.__make_clickthrough(hwnd)
-                self.__disable_input(hwnd)
-                self.__hide_from_taskbar(hwnd)
+        start_time = perf_counter()
+        while perf_counter() - start_time <= self.__thread_timeout:
+            if not self.__is_hidden_hwnd_kernel32:
+                self.__hide(ctypes.windll.kernel32.GetConsoleWindow())
+                self.__is_hidden_hwnd_kernel32 = True
+            if not self.__is_hidden_hwnd_win32gui:
+                hwnd = self.__get_hwnd(self.__console_title)
+                if hwnd is not None and hwnd != 0:
+                    self.__hide(hwnd)
+                    self.__is_hidden_hwnd_win32gui = True
+            if self.__is_hidden_hwnd_kernel32 and self.__is_hidden_hwnd_win32gui:
                 break
+            sleep(0.25)
+            
+    def __hide(self, hwnd):
+        self.__make_fully_transparent(hwnd)
+        self.__make_clickthrough(hwnd)
+        self.__disable_input(hwnd)
+        self.__hide_from_taskbar(hwnd)
 
     def __get_hwnd(self, title):
         return win32gui.FindWindowEx(None, None, None, title) or None
