595e734dfbc7e0d1c2e9bde2c7e124cb45d69df3
ephell <dremaitiserikas@yahoo.com>
2023-11-01 19:00:45 +0200

    Add ConsoleHider() to main.py

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import sys
 
 from src.gui.application.application import Application
 from src.gui.main_window.main_window import MainWindow
+from src.utilities.console_hider import ConsoleHider
 
-    
+
 if __name__ == "__main__":
     freeze_support()
+    console_hider = ConsoleHider()
+    console_hider.start()
     app = Application(sys.argv)
     main_window = MainWindow(app)
     main_window.show()
