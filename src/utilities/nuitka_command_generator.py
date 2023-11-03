10a88e15179ce7ccf536ce8b87fe0b7c00420cd2
ephell <dremaitiserikas@yahoo.com>
2023-11-03 02:58:52 +0200

    Improve docstring; Add qt_plugins_command generator method

--- src/utilities/nuitka_command_generator.py
+++ src/utilities/nuitka_command_generator.py
@@ -2,8 +2,15 @@ import os
 
 
 class NuitkaCommandGenerator:
-    """Utility class for generating a compilation command for Nuitka."""
+    """
+    Utility class for generating a compilation command for Nuitka.
+    
+    - Install 'nuitka' to virtual environment.
+    - Activate virtual environment.
+    - Generate the command.
+    - Run the command.
 
+    """
     _MODE = "--standalone"
     _ENTRY_FILE_NAME = "main.py"
     _APP_ICON_RELATIVE_PATH = "src\\gui\\application\\logo.ico" # Must be .ico file
@@ -15,7 +22,8 @@ class NuitkaCommandGenerator:
         "src\\gui\\main_window\\buttons\\open_editor_button\\editor_window\\timeline\\media_item\\media_item_handle.jpg",
         "src\\gui\\main_window\\buttons\\open_editor_button\\editor_window\\timeline\\media_item\\media_item_handle_in_focus.jpg",
         "src\\gui\\main_window\\buttons\\screenshot_button\\camera-click.wav",
-        "src\\settings\\settings.ini"
+        "src\\settings\\settings.ini",
+        "src\\imageio_ffmpeg\\ffmpeg-win64-v4.2.2.exe"
     ]
     _DATA_DIR_RELATIVE_PATHS = [
         "src\\gui\\main_window\\system_tray\\icons",
@@ -23,22 +31,23 @@ class NuitkaCommandGenerator:
     _PLUGINS_TO_ENABLE = [
         "pyside6"
     ]
+    _QT_PLUGINS = [
+        "multimedia"
+    ]
  
     def generate_final_command(self):
-        mode = self._get_mode()
-        entry_file_name = self._get_entry_file_name()
-        app_icon_command = self._generate_app_icon_command()
-        include_data_files_commands = self._generate_include_data_files_commands()
-        include_data_dir_commands = self._generate_include_data_dir_commands()
-        enable_plugin_commands = self._generate_enable_plugin_commands()
-        command = f"python -m nuitka {mode} {entry_file_name} {app_icon_command} "
-        for include_data_files_command in include_data_files_commands:
-            command += f"{include_data_files_command} "
-        for include_data_dir_command in include_data_dir_commands:
-            command += f"{include_data_dir_command} "
-        for enable_plugin_command in enable_plugin_commands:
-            command += f"{enable_plugin_command} "
-        return command
+        final_command = f"python -m nuitka "
+        final_command += f"{self._get_mode()} "
+        final_command += f"{self._get_entry_file_name()} "
+        final_command += f"{self._generate_app_icon_command()} "
+        final_command += f"{self._generate_include_qt_plugins_command()} "
+        for command in self._generate_include_data_files_commands():
+            final_command += f"{command} "
+        for command in self._generate_include_data_dir_commands():
+            final_command += f"{command} "
+        for enable_plugin_command in self._generate_enable_plugin_commands():
+            final_command += f"{enable_plugin_command} "
+        return final_command
     
     def _get_mode(self):
         return self._MODE
@@ -68,6 +77,12 @@ class NuitkaCommandGenerator:
         for plugin in self._PLUGINS_TO_ENABLE:
             commands.append(f"--enable-plugin={plugin}")
         return commands
+
+    def _generate_include_qt_plugins_command(self):
+        command = f"--include-qt-plugins="
+        for plugin in self._QT_PLUGINS:
+            command += f"{plugin},"
+        return command[:-1]
 
     def _get_absolute_path(self, relative_path):
         return os.path.abspath(relative_path)
